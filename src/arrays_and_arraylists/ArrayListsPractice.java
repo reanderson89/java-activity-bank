a15745b8eb2cffed369381206aece7ddb7bdd839
robAnderson <reanderson89@gmail.com>
2022-06-20 15:00:56 -0700

    added array and arrayLists activites

---
+++ src/arrays_and_arraylists/ArrayListsPractice.java
@@ -0,0 +1,25 @@
+package arrays_and_arraylists;
+
+public class ArrayListsPractice {
+    public static void main(String[] args) {
+//        Create an ArrayList with 5 elements
+//        add 5 elements to it
+//        print the entire ArrayList to the terminal
+//        print the length of it
+//        remove some elements from the ArrayList
+//        print the entire ArrayList again, and it's size
+
+//        Create an ArrayList with 5 elements and give the value in-line
+//        mess around with various methods for ArrayLists. Just type out the variable name and then put a "." and scroll through the list of methods.
+//        read the information it shows you and try to figure out how to use it.
+//        create different conditionals using "if-else" statements to test different methods. For example:
+//        if (myArray.contains("something"){
+//        then do something
+//        }
+//        print out whatever you need to the terminal to check and see if the methods work the way you think they should.
+
+
+
+
+    }
+}
---
+++ src/arrays_and_arraylists/ArraysPractice.java
@@ -0,0 +1,17 @@
+package arrays_and_arraylists;
+
+public class ArraysPractice {
+    public static void main(String[] args) {
+//        Create an array with 5 elements
+//        give each index in the array a value
+//        print the entire array to the terminal
+//        print the length of the array
+
+//        Create an array with 5 elements and give the values in-line
+//        print the entire array to the terminal
+//        print the length of the array
+
+//        change a few values in each array and then print both arrays to the terminal again
+
+    }
+}
