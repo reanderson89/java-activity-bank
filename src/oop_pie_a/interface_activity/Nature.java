7ef3fe1ede3d58e6ef1b05013e955f4c07bc27a6
robAnderson <reanderson89@gmail.com>
2022-06-27 10:29:07 -0700

    created interface activity

---
+++ src/oop_pie_a/interface_activity/Nature.java
@@ -0,0 +1,12 @@
+package oop_pie_a.interface_activity;
+
+public class Nature {
+
+    public static void main(String[] args) {
+//create an instance of your class that you created
+//        use all the methods you created in your class, some you will have to add a print statement for.
+//        to test the default method you will need to assign the return value of getWeightInLbs() to a variable and then pass it into the convertWeightToKgs() method.
+//        also, print out the value of the attribute named finalAndStatic to the terminal. Remember that static variables don't exist on the instance of the class.
+
+    }
+}
