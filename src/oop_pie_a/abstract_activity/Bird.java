551fa626c3423bc42dfe412813ac4fdb75195c9c
robAnderson <reanderson89@gmail.com>
2022-06-27 11:13:15 -0700

    created abstract activity

---
+++ src/oop_pie_a/abstract_activity/Bird.java
@@ -0,0 +1,18 @@
+package oop_pie_a.abstract_activity;
+
+public class Bird {
+String type;
+char gender;
+
+public void eat(){
+    System.out.println("Yummm!");
+}
+
+public void sleep(){
+    System.out.println("zzz...zzz...zzz...");
+}
+
+public void fly(){
+    System.out.println("Soars through the air!");
+}
+}
---
+++ src/oop_pie_a/abstract_activity/Nature.java
@@ -0,0 +1,8 @@
+package oop_pie_a.abstract_activity;
+
+public class Nature {
+
+    public static void main(String[] args) {
+
+    }
+}
